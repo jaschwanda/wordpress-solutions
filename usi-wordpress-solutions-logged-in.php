687bf2dd433535707044e2dd55334ce2c14c7148
jas473@ored.rutgers.edu <jim.schwanda@rutgers.edu>
2022-01-15 12:00:00 -0500

    Added USI_WordPress_Solutions_Logged_In class which uses cookies to verify if any user logged in.

---
+++ usi-wordpress-solutions-logged-in.php
@@ -0,0 +1,34 @@
+<?php // ------------------------------------------------------------------------------------------------------------------------ //
+
+/*
+WordPress-Solutions is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ 
+WordPress-Solutions is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License along with WordPress-Solutions. If not, see 
+https://github.com/jaschwanda/wordpress-solutions/blob/master/LICENSE.md
+
+Copyright (c) 2020 by Jim Schwanda.
+*/
+
+final class USI_WordPress_Solutions_Logged_In {
+
+   const VERSION = '2.12.4 (2022-01-15)';
+
+   private function __construct() {
+   } // __construct();
+
+   public static function logged_in() {
+      foreach ($_COOKIE as $key => $value) {
+         if (substr($key, 0, 20) == 'wordpress_logged_in_') return;
+      }
+      die('Accesss not allowed.');
+   } // versions();
+
+} // Class USI_WordPress_Solutions_Logged_In;
+
+USI_WordPress_Solutions_Logged_In::logged_in();
+
+// --------------------------------------------------------------------------------------------------------------------------- // ?>
