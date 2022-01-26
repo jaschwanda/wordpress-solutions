e8fc38985533a656a0990c16b07c6381c1f96a3a
jas473@ored.rutgers.edu <jim.schwanda@rutgers.edu>
2022-01-26 12:10:00 -0500

    Added illumination variables.

--- usi-wordpress-solutions.php
+++ usi-wordpress-solutions.php
@@ -15,7 +15,7 @@ Requires at least: 5.0
 Requires PHP:      7.0.0
 Tested up to:      5.3.2
 Text Domain:       usi-wordpress-solutions
-Version:           2.12.10
+Version:           2.12.11
 */
 
 /*
@@ -40,7 +40,7 @@ require_once('usi-wordpress-solutions-log.php');
 
 final class USI_WordPress_Solutions {
 
-   const VERSION = '2.12.10 (2022-01-24)';
+   const VERSION = '2.12.11 (2022-01-25)';
 
    const NAME       = 'WordPress-Solutions';
    const PREFIX     = 'usi-wordpress';
