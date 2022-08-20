29160a908d6f8c7267430dbe77d7c1c7be6ba605
jas473@ored.rutgers.edu <jim.schwanda@rutgers.edu>
2022-08-20 12:00:00 -0500

    Optimize loading and execution of settings pages.

--- usi-wordpress-solutions-settings.php
+++ usi-wordpress-solutions-settings.php
@@ -73,7 +73,20 @@ class USI_WordPress_Solutions_Settings {
 
       if ('admin-ajax.php' == $pagenow) return;
 
-      if (!empty($config['prefix'])) $this->prefix = $config['prefix'];
+      if (!empty($config['capability']))   $this->capability   = $config['capability'];
+      if (!empty($config['capabilities'])) $this->capabilities = $config['capabilities'];
+      if (!empty($config['datepicker']))   $this->datepicker   = $config['datepicker'];
+      if (!empty($config['editor']))       $this->editor       = $config['editor'];
+      if (!empty($config['hide']))         $this->hide         = $config['hide'];
+      if (!empty($config['icon_url']))     $this->icon_url     = $config['icon_url'];
+      if (!empty($config['name']))         $this->name         = $config['name'];
+      if (!empty($config['options']))      $this->options      = & $config['options'];
+      if (!empty($config['page']))         $this->page         = $config['page'];
+      if (!empty($config['position']))     $this->position     = $config['position'];
+      if (!empty($config['prefix']))       $this->prefix       = $config['prefix'];
+      if (!empty($config['query']))        $this->query        = $config['query'];
+      if (!empty($config['roles']))        $this->roles        = $config['roles'];
+      if (!empty($config['text_domain']))  $this->text_domain  = $config['text_domain'];
 
       $this->impersonate = !empty(USI_WordPress_Solutions::$options['admin-options']['impersonate']);
 
@@ -88,20 +101,6 @@ class USI_WordPress_Solutions_Settings {
       $this->is_page     = !empty($_GET['page']) && ($_GET['page'] == $this->page_slug) && (('admin.php' == $pagenow) || ('options-general.php' == $pagenow));
 
       $this->is_options  = !empty($_POST['option_page']) && ($_POST['option_page'] == $this->page_slug) && ('options.php' == $pagenow);
-
-      if (!empty($config['capability']))   $this->capability   = $config['capability'];
-      if (!empty($config['capabilities'])) $this->capabilities = $config['capabilities'];
-      if (!empty($config['datepicker']))   $this->datepicker   = $config['datepicker'];
-      if (!empty($config['editor']))       $this->editor       = $config['editor'];
-      if (!empty($config['hide']))         $this->hide         = $config['hide'];
-      if (!empty($config['icon_url']))     $this->icon_url     = $config['icon_url'];
-      if (!empty($config['name']))         $this->name         = $config['name'];
-      if (!empty($config['page']))         $this->page         = $config['page'];
-      if (!empty($config['options']))      $this->options      = & $config['options'];
-      if (!empty($config['position']))     $this->position     = $config['position'];
-      if (!empty($config['query']))        $this->query        = $config['query'];
-      if (!empty($config['roles']))        $this->roles        = $config['roles'];
-      if (!empty($config['text_domain']))  $this->text_domain  = $config['text_domain'];
 
       if ('plugins.php' == $pagenow) {
 
