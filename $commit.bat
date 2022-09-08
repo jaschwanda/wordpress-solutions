74afd63375794ba0927d0b7acfa509a447a0430d
jas473@ored.rutgers.edu <jim.schwanda@rutgers.edu>
2022-09-08 08:48:00 -0400

    Optimize loading and execution of settings pages.

--- $commit.bat
+++
@@ -1,7 +0,0 @@
-git add .
-git commit -m "Optimize loading and execution of settings pages."
-set GIT_COMMITTER_DATE="2022-08-16 12:00:00 EST"
-git commit --amend --no-edit --date "2022-08-16 12:00:00 EST"
-git pull
-git push origin main
- 
