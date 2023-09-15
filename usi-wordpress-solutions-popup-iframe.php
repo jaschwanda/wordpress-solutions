e46d7b23dbe9504b9a1f815b73cffcb2ab01539a
Jim Schwanda <jim.schwanda@rutgers.edu>
2023-09-15 13:02:24 -0400

    Added autoloader for USI plugins, set all versions numbers to same version.

--- usi-wordpress-solutions-popup-iframe.php
+++ usi-wordpress-solutions-popup-iframe.php
@@ -17,7 +17,7 @@ Copyright (c) 2023 by Jim Schwanda.
 
 class USI_WordPress_Solutions_Popup_Iframe {
 
-   const VERSION = '2.16.0 (2023-09-15)'
+   const VERSION = '2.16.0 (2023-09-15)';
 
    private static $attributes = array();
    private static $scripts    = array();
