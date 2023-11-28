d47d54ad5d5fa4f55e9897eef43303dcca2203aa
Jim Schwanda <jim.schwanda@rutgers.edu>
2023-11-28 07:17:18 -0500

    Modified to use proper coding constructs.

--- usi-wordpress-solutions-bootstrap.php
+++ usi-wordpress-solutions-bootstrap.php
@@ -1,20 +1,7 @@
 <?php // ------------------------------------------------------------------------------------------------------------------------ //
 
-/*
-WordPress-Solutions is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
-License as published by the Free Software Foundation, either version 3 of the License, or any later version.
- 
-WordPress-Solutions is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License along with WordPress-Solutions. If not, see 
-https://github.com/jaschwanda/wordpress-solutions/blob/master/LICENSE.md
-
-Copyright (c) 2023 by Jim Schwanda.
-*/
-
 /* const VERSION = '2.16.0 (2023-09-15)'; */
 
-require_once(explode('wp-content', $_SERVER['SCRIPT_FILENAME'])[0] . 'wp-load.php');
+require_once explode('wp-content', $_SERVER['SCRIPT_FILENAME'])[0] . 'wp-load.php';
 
 // --------------------------------------------------------------------------------------------------------------------------- // ?>
